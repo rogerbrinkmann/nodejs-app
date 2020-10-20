d46dc84e76e428ef6af3e972b383eefb1e207bdb
rogerbrinkmann <rogerbrinkmann@gmail.com>
2020-10-20 22:13:28 +0200

    ejs layouts

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
-app.set('view engine', 'ejs');
+app.use(expressLayouts);
 app.use(express.static('public'))
 
+app.set('layout', './layouts/full-width');
+app.set('view engine', 'ejs');
+// app.set('layout', 'views/layouts/default');
+
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: 'Home' });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' , layout:'./layouts/side-bar'});
 })
 
 app.listen(3000);
